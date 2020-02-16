e867390cc67fb46d38bec7294907559c4c1385f2
Lasana Murray <lmurray@quenk.com>
2020-02-16 19:39:26 -0400

    chore: update tests

--- lib/tests.ts
+++ lib/tests.ts
@@ -189,12 +189,12 @@ export const tests: { [key: string]: any } = {
     },
     'should parse in': {
 
-        input: 'id: in [1,2,3,4,5,12]'
+        input: 'uid: in [1,2,3,4,5,12]'
 
     },
     'should parse not in': {
 
-        input: 'id: !in [1,2,3,4,5,12]'
+        input: 'uid: !in [1,2,3,4,5,12]'
 
     }
 
